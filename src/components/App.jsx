18932a25220cef5a2b6c66160e8d7a575dea7fef
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-26 22:11:28 +0300

    container component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
-import Profile from './Profile';
 import './App.module.css';
+import Profile from './Profile';
+import Statistics from './Statistics';
 import user from '../fetched-data/user.json';
+import data from '../fetched-data/data.json';
+import Container from './Container';
 
 export const App = () => {
   return (
@@ -12,15 +15,23 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
+        gap: 40,
       }}
     >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <Container>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Container>
+
+      <Container>
+        <Statistics title="Upload stats" stats={data} />
+        <Statistics stats={data} />
+      </Container>
     </div>
   );
 };
