4e73e74dcf41a6bf582231d93e3e1977d20df49e
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-26 13:48:10 +0300

    container children proptypes were added

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
+
 function Container({ children }) {
   return <div>{children}</div>;
 }
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
 
 export default Container;
