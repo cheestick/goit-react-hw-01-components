63218a946085a0fbf1cf79205367e6b185a9c9a7
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-05-26 22:13:21 +0300

    statistics component added. 2nd task completed

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s['stat-list']}>
+        {stats.map(({ label, percentage, id }) => (
+          <li className={s.item} key={id}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.defaultProps = {
+  title: null,
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
